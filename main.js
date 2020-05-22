430fe78ed2cdfc5c761a06ebd043178b5a6d145e
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2020-05-22 19:50:47 +0530

    Add main.js, mainWindow with basic setup and developer tools

---
+++ main.js
@@ -0,0 +1,60 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+// Set ENV
+// process.env.NODE_ENV = 'production'
+
+let mainWindow;
+
+// Listen for app to ready
+app.on('ready', () => {
+  // Create a new window
+  mainWindow = new BrowserWindow({
+      webPreferences: {
+          nodeIntegration: true
+      }
+  });
+
+  // Load HTML into window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Quit app when closed
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert Menu
+  Menu.setApplicationMenu(mainMenu);
+
+});
+
+// Create menu template
+const mainMenuTemplate = [];
+
+// Add developer tools item if not in prod
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+    {
+      label: 'Toggle Dev Tools',
+      accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+      click(item, focusedWindow){
+      focusedWindow.toggleDevTools();
+      }
+    },
+    {
+      role: 'reload'
+    }
+    ]
+  })
+}

687ef65203f566e5986ca624f8286690edd870ce
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2020-06-11 08:54:52 +0530

    Add company table, dev db, remove sales_entry migration

---
+++ db/migrations/20200611084226_create_company_table.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+  knex.schema.createTable('companies', table => {
+    table.increments()
+    table.string('name').notNullable().unique()
+    table.string('address')
+    table.string('phone')
+    table.string('state')
+    table.string('gst_no')
+    table.string('bank_name')
+    table.string('bank_account_no')
+    table.string('bank_branch_name')
+    table.string('bank_ifsc_code')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('companies');
+};
